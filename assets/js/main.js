f893092f0269ab23be68ef1f2179a3c96e3537ee
Ryan <ryan@thecpafirm.com>
2019-11-18 12:13:36 -0700

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
-function1 = () =>
-{
-    let pos1 = document.querySelector('win-prop')
-    let pos2 = document.querySelector('doc-prop')
+let pos1 = document.querySelector('.winProp')
+let pos2 = document.querySelector('.docProp')
 
-    pos1.textContent=`Your window height is ${window.innerHeight}, and your window width is ${window.innerWidth}.\nYour window offset is ${window.pageXOffset} from the left, and ${window.pageYOffset} from the top.\nThe page URL is ${window.URL}`
-    pos2.textContent=``
+myFunction = () =>
+{
+
+    pos1.textContent=`Your window height is ${window.innerHeight}, and your window width is ${window.innerWidth}. \n Your window offset is ${window.pageXOffset} from the left, and ${window.pageYOffset} from the top.\nThe page URL is ${window.URL}`
+    pos2.textContent=`This document is ${document.title}, located at ${window.location.href}`
 }
